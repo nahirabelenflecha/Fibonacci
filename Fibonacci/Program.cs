7197f5f2006c4b59b6544b1c1b92cb700400255b
nahirabelenflecha <60350810+nahirabelenflecha@users.noreply.github.com>
2020-02-06 09:42:58 -0300

    Add files via upload

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int f1=0, f2=1, cant, aux;
+          string linea;
+
+          Console.WriteLine("¿Cuantos numeros de la serie de Fibonacci desea ver?");
+          linea = Console.ReadLine();
+          cant = int.Parse(linea);
+          
+          for (int i=0; i<cant; i++)
+          {
+            aux = f1;
+            f1 = f2;
+            f2 = aux + f1;
+            Console.WriteLine(f1);
+          }
+        }
+    }
+}
